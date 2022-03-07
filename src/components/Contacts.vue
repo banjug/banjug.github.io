c207093c4e0f91311734966a509a8492fb618498
Samuele <miotto.samuele@gmail.com>
2022-03-07 15:23:01 +0100

    add contacts section

--- src/components/Contacts.vue
+++ src/components/Contacts.vue
@@ -1,5 +1,24 @@
 <template>
-  <section></section>
+  <section>
+    <div class="container">
+      <p>Ti piace quello che faccio?</p>
+      <p>Contattami a questi link. A presto!</p>
+      <div>
+        <a href="https://github.com/samuelemiotto">
+          <font-awesome-icon icon="fa-brands fa-github" />
+          <span>Github</span>
+        </a>
+        <a href="https://www.linkedin.com/in/samuele-miotto/">
+          <font-awesome-icon icon="fa-brands fa-linkedin" />
+          <span>LinkedIn</span>
+        </a>
+        <a href="mailto:mioto.samuele@gmail.com?subject=Conosciamoci">
+          <font-awesome-icon icon="fa-solid fa-envelope" />
+          <span>Email</span>
+        </a>
+      </div>
+    </div>
+  </section>
 </template>
 <script>
 export default {
@@ -11,7 +30,47 @@ export default {
 </script>
 <style lang="scss" scoped>
 section {
-  height: 50vh;
-  background: blue;
+  background: #111;
+  display: flex;
+  justify-content: center;
+  .container {
+    width: 70%;
+    display: flex;
+    flex-direction: column;
+    padding: 5rem 0;
+    p {
+      color: #ffafcc;
+      font-family: "DM Mono", monospace;
+      font-size: 2rem;
+      width: 60%;
+    }
+    div {
+      display: flex;
+      justify-content: center;
+      padding: 5rem 0 0;
+      a {
+        color: #ffafcc;
+        font-family: "DM Mono", monospace;
+        font-size: 4rem;
+        margin-right: 2rem;
+        transition: all 0.2s ease-in-out;
+        display: flex;
+        flex-direction: column;
+        span {
+          opacity: 0;
+          font-size: 1rem;
+          margin-top: 0.5rem;
+          transition: all 0.2s ease-in-out;
+          text-align: center;
+        }
+        &:hover {
+          color: #fff;
+          span {
+            opacity: 1;
+          }
+        }
+      }
+    }
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import specific icons */
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faFile } from "@fortawesome/free-solid-svg-icons";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* add icons to the library */
+library.add(faGithub, faLinkedin, faEnvelope, faFile);
+
+/* add font awesome icon component */
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
